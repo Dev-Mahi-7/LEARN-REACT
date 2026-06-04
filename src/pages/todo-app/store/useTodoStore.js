da48559929191f8cf40808ffdb52aee020f4346d
Dev-Mahi-7 <developermahi9@gmail.com>
2026-06-04 22:36:58 +0530

    feat:"successfully done store data in localstorage"

--- src/pages/todo-app/store/useTodoStore.js
+++ src/pages/todo-app/store/useTodoStore.js
@@ -1,38 +1,46 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useTodoStore = create((set) => ({
-  todos: [],
-  filter: "all",
-  setFilter: (filter) => set({ filter }),
+export const useTodoStore = create(
+  persist(
+    (set) => ({
+      todos: [],
+      filter: "all",
+      setFilter: (filter) => set({ filter }),
 
-  addTodo: (text) =>
-    set((state) => ({
-      todos: [
-        ...state.todos,
-        {
-          id: Date.now(),
-          text,
-          completed: false,
-        },
-      ],
-    })),
+      addTodo: (text) =>
+        set((state) => ({
+          todos: [
+            ...state.todos,
+            {
+              id: Date.now(),
+              text,
+              completed: false,
+            },
+          ],
+        })),
 
-  deleteTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
+      deleteTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
 
-  toggleTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    })),
+      toggleTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+          ),
+        })),
 
-  editTodo: (id, text) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, text } : todo,
-      ),
-    })),
-}));
+      editTodo: (id, text) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, text } : todo,
+          ),
+        })),
+    }),
+    {
+      name: "todo-storage", // localStorage key
+    },
+  ),
+);
